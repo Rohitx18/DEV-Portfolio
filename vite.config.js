6c52c85f02f727813bc3a2f211dc87083f238e11
Rohit N <nrohit907@gmail.com>
2026-04-17 16:38:10 +0530

    Updated dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // if using React
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/DEV-Portfolio/',  // 👈 THIS is the key fix
+  base: process.env.VITE_BASE_PATH || '/',
 })
